4e5c0a829e8f126f4a955dcc33564aded54c7b89
Prathamesh Juvatkar <prathamesh.juvatkar@gmail.com>
2019-12-17 16:06:00 -0800

    Changed dataset info

--- research/deeplab/datasets/data_generator.py
+++ research/deeplab/datasets/data_generator.py
@@ -102,11 +102,11 @@ _ADE20K_INFORMATION = DatasetDescriptor(
 
 _NANONETS_SHERWIN_WILLIAMS = DatasetDescriptor(
     splits_to_sizes={
-        'train': 198,
-        'val': 25,
-        'test': 25,
+        'train': 197,
+        'val': 24,
+        'test': 24,
     },
-    num_classes=2,
+    num_classes=4,
     ignore_label=255,
 )
 
